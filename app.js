d2a893507c5c48f9c7e47ee8ca79bc6194e906e6
arafat-alim <arafat97alim@gmail.com>
2025-02-09 01:11:11 +0530

    ✨ Middlewares implemented: Added Global errorMiddleware and express native middlewares.

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import { PORT } from "./config/env.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectingToDatabase from "./config/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API!");
@@ -14,6 +20,8 @@ app.get("/", (req, res) => {
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware());
 
 app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
